54118d72f4f7886366aa844214c08c92ac74778b
himash1997 <31865243+himash1997@users.noreply.github.com>
2020-03-21 12:03:49 +0530

    Initial Commit

---
+++ Grocery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Grocery
+//
+//  Created by Himash Nadeeshan on 3/21/20.
+//  Copyright © 2020 Himash Nadeeshan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
